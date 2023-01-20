de4315ffe62a008b0b26ab7744f06dc4b2721211
ukrwebprom <ukrwebprom@gmail.com>
2023-01-20 15:14:45 +0000

    Deploying to gh-pages from @ ukrwebprom/Warm-Lamp-Char@cbd89994921de102786dd54f944fed57ee0e7a91 🚀

--- index.021efaf3.js
+++
@@ -1,2 +0,0 @@
-const e=window.location.search;console.log("queryString: ",e);const t=document.querySelector("#typearea");t.addEventListener("keydown",(e=>{if("Enter"===e.key){e.preventDefault();const o=t.textContent.trim();""!==o&&(n=o,console.log(n),t.textContent="")}var n}));
-//# sourceMappingURL=index.021efaf3.js.map
