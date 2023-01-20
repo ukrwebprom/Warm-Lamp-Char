e829891623bfb78b37aea3a62c2c23fde67fcbc8
ukrwebprom <ukrwebprom@gmail.com>
2023-01-20 12:17:34 +0200

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 /* import _ from 'lodash.throttle'; */
+const queryString = window.location.search;
+console.log(queryString);
 
 const typearea = document.querySelector('#typearea');
 typearea.addEventListener('keydown', evt => {
